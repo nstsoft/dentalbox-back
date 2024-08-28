8eeac5633d61f4ff622c9f0a58e83818f89c2c0a
Oleg <keeperoleg26@gmial.com>
2024-08-28 18:38:11 +0300

    chore(lambda sns permissions)

--- aws/lib/stack.ts
+++ aws/lib/stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 import { S3ImagesBucketConstruct } from './bucket';
 import { ApiGateway } from './gateway';
 import { ApiConstruct } from './lambdas';
 import { SharpLayerConstruct } from './sharp-layer';
-
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -13,6 +13,8 @@ export class AwsStack extends cdk.Stack {
     const { layer } = new SharpLayerConstruct(this, 'sharp-layer');
     const { imagesBucket } = new S3ImagesBucketConstruct(this, 'images-bucket');
     const { handler } = new ApiConstruct(this, 'dentalbox-api', [layer]);
+
+    handler.addToRolePolicy(new PolicyStatement({ actions: ['sns:Publish'], resources: ['*'] }));
 
     imagesBucket.grantWrite(handler);
     imagesBucket.grantPut(handler);
